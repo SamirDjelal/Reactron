5e1c0f6932f2d96f89c1fca531133e35dc257e1e
Samir Djelal <samir.djelal.webdesign@gmail.com>
2020-04-23 16:44:06 +0100

    Manu check pathname.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,20 +3,24 @@ import {Link} from 'react-router-dom';
 import {withRouter} from "react-router";
 
 class Menu extends Component {
+	
 	render() {
+		
+		const pathname = this.props.location.pathname;
+		
 		return (
 			<div className="Menu">
 				
 				<Link to="/">
 					
-					<div className={"item home " + (this.props.location.pathname === '/' && 'active')}
+					<div className={"item home " + (pathname === '/' && 'active')}
 					     title="Home"/>
 				
 				</Link>
 				
 				<Link to="/setting">
 					
-					<div className={"item setting " + (this.props.location.pathname === '/setting' && 'active')}
+					<div className={"item setting " + (pathname === '/setting' && 'active')}
 					     title="Setting"/>
 				
 				</Link>
@@ -24,6 +28,7 @@ class Menu extends Component {
 			</div>
 		);
 	}
+	
 }
 
 export default withRouter(Menu);
