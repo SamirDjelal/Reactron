633fcaecbc8c6eec362552c5b825cf5f5e3824f9
Toxic Hydra <toxichydra@icloud.com>
2020-04-25 11:41:13 +0100

    activation screen

---
+++ src/screens/Activation.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Activation extends Component {
+	render() {
+		return (
+			<div>
+				Activation
+			</div>
+		);
+	}
+}
+
+export default Activation;
